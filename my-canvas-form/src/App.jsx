909d5bdd6dfd66a9ccf81ac111d4db6c27b3d9da
axeave <ss_ono@icloud.com>
2025-05-17 19:45:53 +0900

    for render

--- my-canvas-form/src/App.jsx
+++ my-canvas-form/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     let timeoutId;
 
     const fetchLines = () => {
-      fetch(`${process.env.REACT_APP_API_URL}/api/get_all_sketch_lines`)
+      fetch(`${import.meta.env.VITE_API_URL}/api/get_all_sketch_lines`)
         .then(res => res.json())
         .then(data => {
           // ① sketch_id ごとにグループ化
